73a168749d17a17c80c5b13e9b6a71a2437a8a14
Gabriel Thome da Cruz Schirmer <gabriel.schirmer@compasso.com.br>
2021-11-16 12:09:03 -0300

    fixing UI

--- sudokuu.js
+++ sudokuu.js
@@ -1,51 +1,85 @@
+(function () {
+    var table = document.getElementById("table");
+
+    var x;
+    var y;
+    var d = '';
+
+    for (y = 0; y < 9; y++) {
+        d += "<tr>";
+        for (x = 0; x < 9; x++) {
+            d += "<td>" + "<input type='number' class='form-control'  max='9' id='" + y + ','+ x + "'>" + "</td>";
+        }
+        d += "</tr>";
+    }
+    table.innerHTML = d;
+
+})();
+
+
+
 var sudokuTeste = [
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
+function resolverSudoku() {
+    for (let i = 0 ; i < 9 ; i++) {
+        for (let j = 0 ; j < 9 ; j++) {
+            const valor = document.getElementById(i + ',' +j).value
+            if(valor===''){
+                sudokuTeste[i][j] = 0
+            } else if (valor !== ''){
+                
+                sudokuTeste[i][j] = valor
+            }
+        }    
+    }
+    bruteForce(sudokuTeste)
+} 
 
-function gerarSudoku(){
-    (function () {
-        var app = document.getElementById("app");
-        let table = document.createElement('table');
-        app.appendChild(table)
-        table.setAttribute('id' , 'tabela_sudoku')
-    
-        for (let i = 0; i < 9; i++) {
-            let tableRow = document.createElement('tr')
-            table.appendChild(tableRow)
-          for (let x = 0; x < 9; x++) {
-          }
-        }
-    
-      })();
-}
+// function gerarSudoku() {
+//     (function () {
+//         var app = document.getElementById("app");
+//         let table = document.createElement('table');
+//         app.appendChild(table)
+//         table.setAttribute('id', 'tabela_sudoku')
+
+//         for (let i = 0; i < 9; i++) {
+//             let tableRow = document.createElement('tr')
+//             table.appendChild(tableRow)
+//             for (let x = 0; x < 9; x++) {
+//             }
+//         }
+
+//     })();
+// }
 
 
 function print_board(board) {
 
-    var i , j;
-    for (i = 0; i < board.length ; i++){
-        if ((i % 3 == 0) && (i !=0)){
+    var i, j;
+    for (i = 0; i < board.length; i++) {
+        if ((i % 3 == 0) && (i != 0)) {
             console.log("- - - - - - - - - - - - - ")
         }
 
-        for (j = 0; j < board.length ; j++){
-            if (j % 3 == 0 && j != 0){
-                process.stdout.write(" | ")
+        for (j = 0; j < board.length; j++) {
+            if (j % 3 == 0 && j != 0) {
+                console.log(" | ")
             }
-            if (j==8){
+            if (j == 8) {
                 console.log(board[i][j]);
             } else {
-                process.stdout.write(board[i][j] + " ")    
-                }
+                console.log(board[i][j] + " ")
+            }
         }
     }
 }
@@ -53,32 +87,32 @@ function print_board(board) {
 
 // 0 é vazio
 function procurarEspacoVazio(board) {
-    for (let i = 0; i < 9 ; i++){
-        for (let j = 0; j < 9 ; j++){
-            if (board[i][j] === 0){
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            if (board[i][j] === 0) {
                 //retorna a indexação do espaço vazio
-                return [i , j]
+                return [i, j]
             }
         }
     }
-    return [-1 , -1];
+    return [-1, -1];
 }
 
-function checarSolucao (board , linha , coluna , numero) {
+function checarSolucao(board, linha, coluna, numero) {
 
     let vetorVerificador = []
 
 
-    for (let i = 0 ; i < board[linha].length ; i++) {
+    for (let i = 0; i < board[linha].length; i++) {
         //checar linhas
-        if ((board[linha][i] === numero)){
+        if ((board[linha][i] === numero)) {
             vetorVerificador.push(false)
 
         }
     }
-    for (let i = 0 ; i < board.length ; i++) {
+    for (let i = 0; i < board.length; i++) {
         //checar colunas
-        if ((board[i][coluna] === numero)){
+        if ((board[i][coluna] === numero)) {
             vetorVerificador.push(false)
         }
     }
@@ -88,9 +122,9 @@ function checarSolucao (board , linha , coluna , numero) {
     caixa_x = Math.floor(linha / 3) * 3
     caixa_y = Math.floor(coluna / 3) * 3
 
-    for(let i = 0 ; i < 3; i++){
-        for(let j = 0 ; j < 3 ; j++){
-            if((board[caixa_x + i][caixa_y + j] === numero)){
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if ((board[caixa_x + i][caixa_y + j] === numero)) {
                 vetorVerificador.push(false)
             }
         }
@@ -101,11 +135,11 @@ function checarSolucao (board , linha , coluna , numero) {
     return vetorVerificador
 }
 
-function checarLinha(board , linha , coluna , numero) {
+function checarLinha(board, linha, coluna, numero) {
 
-        for (let i = 0 ; i < board[linha].length ; i++) {
+    for (let i = 0; i < board[linha].length; i++) {
         //checar linhas
-        if ((board[linha][i] === numero)){
+        if ((board[linha][i] === numero)) {
             return false
 
         }
@@ -116,34 +150,58 @@ function checarLinha(board , linha , coluna , numero) {
 
 function bruteForce(board) {
     let posicao = procurarEspacoVazio(board)
-    let linha, 
+    let linha,
         coluna
 
 
-        linha = posicao[0]
-        coluna = posicao[1]
+    linha = posicao[0]
+    coluna = posicao[1]
 
-        if (coluna === -1) {
-            return board
-        }
+    if (coluna === -1) {
+        return board
+    }
 
 
 
-    for (let i=1; i <= 9 ; i++) {
+    for (let i = 1; i <= 9; i++) {
         //console.log(checarSolucao(board, linha , coluna, i))
         let ver = checarSolucao(board, linha, coluna, i)
-        if(ver[0] === true){
+        if (ver[0] === true) {
             board[linha][coluna] = i
-            bruteForce(board);            
+            bruteForce(board);
         }
     }
 
-    if (procurarEspacoVazio(board)[0] !== -1){
+    if (procurarEspacoVazio(board)[0] !== -1) {
         board[linha][coluna] = 0;
     }
 
-    return board
+    atualizarInterface(board)
 }
-let solved
-solved = bruteForce(sudokuTeste)
-print_board(solved);
+
+function atualizarInterface (board) {
+    for (let i = 0 ; i < 9 ; i++) {
+        for (let j = 0 ; j < 9 ; j++) {
+            const valor = document.getElementById(i + ',' +j)
+            valor.value = board[i][j];
+            valor.readOnly=true
+        }    
+    }
+    let botaoLimpar = document.getElementById('botao-limpar')
+    botaoLimpar.disabled = false
+}
+
+function limparTabela(){
+    for (let i = 0 ; i < 9 ; i++) {
+        for (let j = 0 ; j < 9 ; j++) {
+            const valor = document.getElementById(i + ',' +j)
+            valor.value = ''
+            valor.readOnly=false
+        }    
+    }
+    let botaoLimpar = document.getElementById('botao-limpar')
+    botaoLimpar.disabled = true
+}
+// let solved
+// solved = bruteForce(sudokuTeste)
+// print_board(solved);
